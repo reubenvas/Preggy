9cd0d50026c1c18065c2000d3b24b87e8bfc6f96
Rae Williams <syrae.williams@appliedtechnology.se>
2019-04-03 15:53:39 +0200

    added calculating pregnancy fucntionality to server

--- Preggy/services/preggyService.js
+++ Preggy/services/preggyService.js
@@ -1,5 +1,5 @@
 import config from '../config';
-//"2018-01-01"
+
 async function getWeek(num) {
     return fetch(config.backendUrl + `/api/week/${num}`)
     .then(res => res.json())
--- Server/calendar/index.js
+++ Server/calendar/index.js
@@ -1,7 +1,33 @@
-function calculateDueDate( ) {
-    return 'due date';
+function calculateDueDate(date) {
+    const lastPeriod = new Date(date);
+    const dueDate = new Date(lastPeriod.setTime( lastPeriod.getTime() + 280 * 86400000 ));
+    return dueDate;
+}
+
+function calculateWeek(dueDate) {
+    const today = new Date();
+    const dateDiff = (dueDate - today) / 86400000;
+    const daysRemaining = Math.floor(dateDiff);
+    const daysPassed = 280 - daysRemaining;
+    const weeksPassed = Math.ceil(daysPassed/7);
+
+    return weeksPassed;
+}
+
+function calculateAdditionalDays(dueDate){
+    const today = new Date();
+    const dateDiff = (dueDate - today) / 86400000;
+    const daysRemaining = Math.ceil(dateDiff);
+    const daysPassed = 280 - daysRemaining;
+    const weeksPassed = Math.floor(daysPassed/7);
+    const remainder = (daysPassed % 7) - 1;
+
+    return `${weeksPassed} + ${remainder}`;
+    //return '15 + 7';
 }
 
 module.exports = {
-    calculateDueDate
+    calculateDueDate,
+    calculateWeek,
+    calculateAdditionalDays,
 }
